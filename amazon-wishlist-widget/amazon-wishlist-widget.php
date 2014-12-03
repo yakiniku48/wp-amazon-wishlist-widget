d162a61aace8061697dbfed5652948727da05ec7
yakiniku48 <hideyuki.motoo@gmail.com>
2014-12-03 11:26:26 +0900

    Bugs fixed.

--- amazon-wishlist-widget/amazon-wishlist-widget.php
+++ amazon-wishlist-widget/amazon-wishlist-widget.php
@@ -5,7 +5,7 @@ Plugin URI: https://github.com/yakiniku48/WordPress-Amazon-Wishlist-Widget
 Description: Just an Amazon Wishlist Widget.
 Author: Hideyuki Motoo
 Author URI: http://motoo.net/
-Version: 0.1
+Version: 0.2
 */
 
 class WP_AW_Widget extends WP_Widget {
@@ -33,7 +33,7 @@ class WP_AW_Widget extends WP_Widget {
 		$wishlist_id = apply_filters( 'widget_body', $instance['wishlist'] );
 		$locale = apply_filters( 'widget_body', $instance['locale'] );
 		
-		$last_modified = (int) get_option( '_plugin_amazon_wishlist_widget_time' );
+		$last_modified = (int) get_option( '_plugin_amazon_wishlist_widget_time_' . $wishlist_id );
 		if (time() - $last_modified > 60 * 5) {	//every 5min
 			do {
 				$json = $this->get_wishlist( $wishlist_id, $locale, $multipage_start, $multipage_count );
@@ -41,10 +41,10 @@ class WP_AW_Widget extends WP_Widget {
 				$wishlist = array_merge( $wishlist, $json->results );
 				$multipage_start += $multipage_count;
 			} while ( $json->NumRecords > count($wishlist) );
-			update_option( '_plugin_amazon_wishlist_widget_data', $wishlist );
-			update_option( '_plugin_amazon_wishlist_widget_time', time() );
+			update_option( '_plugin_amazon_wishlist_widget_data_' . $wishlist_id, $wishlist );
+			update_option( '_plugin_amazon_wishlist_widget_time_' . $wishlist_id, time() );
 		} else {
-			$wishlist = get_option( '_plugin_amazon_wishlist_widget_data' );
+			$wishlist = get_option( '_plugin_amazon_wishlist_widget_data_' . $wishlist_id );
 		}
 
 		echo $args['before_widget'];
@@ -59,7 +59,7 @@ class WP_AW_Widget extends WP_Widget {
 		<?php
 		foreach ( $wishlist as $item)
 		{
-			$href = rtrim($item->DetailPageURL).'/?tag='.(($tracking_id) ? $tracking_id : 'motoonet-22');
+			$href = rtrim($item->DetailPageURL).'/?tag='.(($tracking_id) ? $tracking_id : 'yakiniku48-22');
 			?>
 			<li>
 				<dl>
@@ -131,10 +131,12 @@ class WP_AW_Widget extends WP_Widget {
 		<?php
 	}
 	public function update( $new_instance, $old_instance ) {
+		delete_option( '_plugin_amazon_wishlist_widget_time_' . $new_instance['wishlist'] );
 		return $new_instance;
 	}
 
 	protected function get_wishlist( $list_id, $locale, $multipage_start, $multipage_count ) {
+		
 		$query .= 'http://ws-fe.amazon-adsystem.com/widgets/q';
 		$query .= '?Operation=GetResults';
 		$query .= '&ListId='.$list_id;
@@ -146,7 +148,10 @@ class WP_AW_Widget extends WP_Widget {
 		$query .= '&ServiceVersion=20070822';
 		$query .= '&MarketPlace='.$locale;
 		
-		$jsonp = file_get_contents( $query );
+		$jsonp = @file_get_contents( $query );
+		
+		if (! $jsonp) return false;
+		
 		if ( preg_match( '|^[\s\w]+\((.+)\)|', $jsonp, $matches ) )
 		{
 			$json = $matches[1];
---
+++ amazon-wishlist-widget/readme.txt
@@ -0,0 +1,38 @@
+=== Amazon Wishlist Widget ===
+Contributors: yakiniku48
+Tags: amazon, widget, sidebar
+Requires at least: 3.8
+Tested up to: 4.0.1
+Stable tag: 0.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+This widget plug-in will display your amazon wishlist in your sidebar.
+
+You can set some options:
+
+* Title
+* Wishlist ID
+* Associate ID
+* Market Place
+
+== Installation ==
+
+1. Extract the contents of the zip file into your '/wp-content/plugins/' directory.
+1. Activate the plugin.
+1. Go to 'Appearance' -> 'Widgets' and add 'Amazon Wishlist Widget' to one of areas.
+1. Set some options of the widget (Wishlist ID, Market Place and more).
+
+== Screenshots ==
+
+1. screenshot-1.png
+
+== Changelog ==
+
+= 0.2 =
+* Bugs fixed.
+
+= 0.1 =
+* First release.
